56657c2d63999150cb257c4b331f71ff73578aad
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2020-04-08 16:41:04 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record){
+  let year 
+  record.find(function(recordObj){
+    if(recordObj["result"] === "W"){
+      year = recordObj.year
+    }
+  })
+  return year
+}
